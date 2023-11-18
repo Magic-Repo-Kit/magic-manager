d93c8e1621c9205a882f2682378022b597418924
tanzl <319403451@qq.com>
2023-11-18 17:54:36 +0800

    登陆页面适配手机端

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import LoginForm from './loginForm';
+import LoginFormPhone from './loginForm/form-phone';
 import MrFooter from '@/components/mr-footer';
 import './index.scss';
 import mrkLogo from '@/assets/images/mrk-dark.png';
@@ -21,33 +22,24 @@ function Login() {
   }, [mode]);
   return (
     <div className="login-container flx-center">
+      {/* PC端登陆页面 */}
       <div className="login-form-bc flx-center">
         {lines.map((index) => (
           <div key={index} className="line"></div>
         ))}
+        {/* 左边背景 */}
         <div className="login-form-left">
-          <img
-            src={loginBc1}
-            style={{
-              width: '500px',
-              marginRight: '150px',
-            }}
-          />
+          <img src={loginBc1} />
         </div>
+        {/* 登陆页面 */}
         <LoginForm />
       </div>
-
-      <Header
-        style={{
-          width: '50%',
-          position: 'fixed',
-          top: '0',
-          background: 'transparent',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
+      {/* 手机端登陆页面 */}
+      <div className="login-form-phone flx-center">
+        <LoginFormPhone />
+      </div>
+      {/* 头部 */}
+      <Header className="login-header">
         <img src={mrkLogo} alt="" height="20" />
         <div>
           <Tooltip title="全屏" color="transparent">
@@ -68,7 +60,7 @@ function Login() {
           </a>
         </div>
       </Header>
-
+      {/* 底部说明 */}
       <Footer
         style={{
           position: 'fixed',
