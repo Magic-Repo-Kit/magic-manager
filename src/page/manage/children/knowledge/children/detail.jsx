56f24c89260b2d171ff25e242dffa7935876434a
tanzhiliang <319403451@qq.com>
2024-01-19 18:51:40 +0800

    fix(manage/knowledge): 知识库详情

--- src/page/manage/children/knowledge/children/detail.jsx
+++ src/page/manage/children/knowledge/children/detail.jsx
@@ -1,68 +1,84 @@
 import React, { useState } from 'react';
 import '../index.scss';
 
+import MrModal from '@/components/mr-modal';
+import MrPagination from '@/components/mr-pagination';
 // 图片
-import knowledgeFile from '@/assets/images/file.png';
-import knowledgeIcon from '@/assets/images/knowledge-icon.png';
+import knowledgeChoose from '@/assets/images/choose.png';
+
 // antd组件
-import { message, Button, Dropdown, Empty, Input } from 'antd';
+import { Button, Radio, Space } from 'antd';
 
 function Detail() {
-  const [dropdownAddOpen, setDropdownAddOpen] = useState(false); //新建下拉状态
+  const [isActive, setIsActive] = useState(1);
+  const [isOpen, setIsOpen] = useState(false); //弹框状态
+
+  // 弹框确认
+  const handleOk = () => {
+    console.log(11121);
+  };
+  // 弹框确认
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setIsActive(e.target.value);
+  };
+
   return (
     <div className="knowledge-detail">
       <div className="knowledge-detail-title">
-        <Dropdown
-          dropdownRender={() => (
-            <div className="knowledge-list-dropdown-box">
-              {/* 新建文件夹 */}
-              <div>
-                <Button
-                  icon={
-                    <>
-                      <img src={knowledgeFile} />
-                    </>
-                  }
-                  type="text"
-                  className="title-dropdown-btn"
-                  onClick={() => {
-                    setDropdownAddOpen(false);
-                    // handleModal({}, 1);
-                  }}
-                >
-                  文件夹
-                </Button>
-              </div>
-              {/* 新建知识库 */}
-              <div>
-                <Button
-                  icon={
-                    <>
-                      <img src={knowledgeIcon} />
-                    </>
-                  }
-                  type="text"
-                  className="title-dropdown-btn"
-                  onClick={() => {
-                    setDropdownAddOpen(false);
-                    // handleModal({}, 2);
-                  }}
-                >
-                  知识库
-                </Button>
-              </div>
-            </div>
-          )}
-          placement="bottomRight"
-          trigger={['click']}
-          open={dropdownAddOpen}
-          onOpenChange={(dropdownAddOpen) =>
-            setDropdownAddOpen(dropdownAddOpen)
-          }
+        <Button
+          type="primary"
+          onClick={() => {
+            setIsOpen(true);
+          }}
         >
-          <Button type="primary">新建</Button>
-        </Dropdown>
+          导入
+        </Button>
       </div>
+
+      <main></main>
+      {/* 弹框 */}
+      <MrModal
+        title={
+          <div className="knowledge-list-modal-title">
+            <img
+              src={knowledgeChoose}
+              style={{ height: '22px', marginRight: '7px' }}
+            />
+            <span>选择来源</span>
+          </div>
+        }
+        content={
+          <div style={{ margin: '20px 0 25px 0' }}>
+            <Radio.Group
+              onChange={onChange}
+              value={isActive}
+              style={{ width: '100%' }}
+            >
+              <div className="knowledge-detail-radio-box user-select">
+                <Radio className="knowledge-detail-radio-item" value={1}>
+                  <div className="knowledge-detail-radio-content">本地文件</div>
+                </Radio>
+
+                <Radio className="knowledge-detail-radio-item" value={2}>
+                  网页链接
+                </Radio>
+
+                <Radio className="knowledge-detail-radio-item" value={3}>
+                  自定义文本
+                </Radio>
+              </div>
+            </Radio.Group>
+          </div>
+        }
+        open={isOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={550}
+      />
     </div>
   );
 }
